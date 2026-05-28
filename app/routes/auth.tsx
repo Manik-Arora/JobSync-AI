d897bdb3a47c47090a7e6e3e87537343610fb470
Manik-Arora <manikarora2605@gmail.com>
2026-05-28 12:47:41 +0530

    Adding resume feedback page

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -4,7 +4,7 @@ import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
   { title: "JobSync AI | Auth" },
-  { name: "description", current: "Log into your account" },
+  { name: "description", content: "Log into your account" },
 ];
 
 const auth = () => {
